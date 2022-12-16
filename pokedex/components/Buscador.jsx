4e121353e84c5f14a3e92099fc9f7f757a7b7cb4
Ultron <duartemattias3@gmail.com>
2022-12-16 14:22:10 +0100

    refactorizacion

--- pokedex/components/Buscador.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const Buscador = () => {
-  return (
-    <div>Buscador</div>
-  )
-}
--- pokedex/hooks/usePokemon.jsx
+++
@@ -1,6 +0,0 @@
-import { useContext } from "react";
-import PokemonContext from "../context/PokemonProvider";
-
-export const usePokemon = () => {
-  return useContext(PokemonContext)
-}
